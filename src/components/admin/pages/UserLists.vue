6dd4718f4a43c62750acd168a95f5da382bec622
mitchspiron <mitchspiron280@gmail.com>
2022-11-01 21:02:03 +0300

    update: user list

--- src/components/admin/pages/UserLists.vue
+++ src/components/admin/pages/UserLists.vue
@@ -14,6 +14,19 @@
         />
       </div>
     </div>
+    <div class="col-3 card border-0 shadow-sm">
+      <div class="input-group">
+        <span class="input-group-text" id="basic-addon1"
+          ><i class="fa fa-search"></i
+        ></span>
+        <select class="form-select" aria-label="Default select example">
+          <option value="" selected disabled>Rôles</option>
+          <option value="1">Administrateur</option>
+          <option value="2">Rédacreur</option>
+          <option value="3">Visiteur</option>
+        </select>
+      </div>
+    </div>
     <div class="">
       <router-link to="/utilisateur/nouveau" class="btn btn-outline-secondary"
         ><i class="fa-solid fa-plus"></i
@@ -26,7 +39,6 @@
         <tr>
           <th>ID</th>
           <th>Nom Complet / E-mail</th>
-          <th>Type</th>
           <th>Rôle</th>
           <th>Actions</th>
         </tr>
@@ -48,13 +60,10 @@
               </div>
             </div>
           </td>
-          <td>
-            <p class="text-muted mb-0 text-center">Particulier</p>
-          </td>
-          <td>
+          <td class="col-md-1">
             <span
               class="d-flex justify-content-center badge bg-success rounded-pill d-inline"
-              >admin</span
+              >Administrateur</span
             >
           </td>
           <td>
